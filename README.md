5e786ca2c0fcdbf05796ab4201c46bf5d50d39b5
i343281 <samuel.davies@sap.com>
2024-08-09 11:54:48 +0100

    feat(base): :sparkles: add base functionality of extension

--- README.md
+++ README.md
@@ -1,71 +1,29 @@
-# climbr README
+# Stand Up Reminder Extension for Visual Studio Code
 
-This is the README for your extension "climbr". After writing up a brief description, we recommend including the following sections.
+The Stand Up Reminder extension for Visual Studio Code helps you maintain a healthier work routine by reminding you to stand up and take breaks at regular intervals while working on your code.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+- **Customizable Intervals**: Configure how many minutes you want to sit and stand according to your preferences.
 
-For example if there is an image subfolder under your extension project workspace:
+- **Timely Reminders**: Get notified when it's time to stand up and take a break, helping you avoid long periods of continuous sitting.
 
-\!\[feature X\]\(images/feature-x.png\)
+## Usage
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+1. Install the Climbr - Stand Up Reminder extension from the Visual Studio Code Marketplace.
 
-## Requirements
+2. Configure your sitting and standing intervals by going to `File` -> `Preferences` -> `Settings` and searching for "Climbr".
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+3. Start working on your code, and let the Stand Up Reminder extension take care of reminding you to stand up and take breaks at regular intervals.
 
-## Extension Settings
+## Configuration
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+You can configure the following settings for the Stand Up Reminder extension:
 
-For example:
+- `climbr.sittingInterval`: The number of minutes you want to sit before receiving a reminder to stand up. Default is 30 minutes.
 
-This extension contributes the following settings:
+- `climbr.standingInterval`: The number of minutes you want to stand before receiving a reminder to sit down. Default is 5 minutes.
 
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+## Feedback
 
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+If you have any feedback or suggestions for the Stand Up Reminder extension, please feel free to [open an issue](https://github.com/draysams/climbr/issues) on GitHub.
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,169 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export const DEFAULT_SITTING_INTERVAL = 1; // Updated sitting interval to 5 minutes for demonstration
+export const DEFAULT_STANDING_INTERVAL = 1;
+export function activate(context: vscode.ExtensionContext): void {
+  const INACTIVE_THRESHOLD = 30;
+  let sittingInterval: number =
+    vscode.workspace.getConfiguration().get('climbr.sittingInterval') ??
+    DEFAULT_SITTING_INTERVAL; // Default to 5 minutes
+  let standingInterval: number =
+    vscode.workspace.getConfiguration().get('climbr.standingInterval') ??
+    DEFAULT_STANDING_INTERVAL; // Default to 1 minute
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "climbr" is now active!');
+  let isUserActive = true;
+  let lastActiveTime = Date.now();
+  let standingUpInterval: NodeJS.Timeout | undefined;
+  let statusBarItem: vscode.StatusBarItem;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('climbr.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from climbr!');
-	});
+  let isAlreadyStanding = false;
+  let standingTimer: NodeJS.Timeout | undefined;
+  const getCurrentDateTime = (): string => {
+    return new Date().toLocaleTimeString();
+  };
 
-	context.subscriptions.push(disposable);
+  const getStatusBarText = (minutes: number, isSitting: boolean): string => {
+    const minuteText = minutes === 1 ? 'minute' : 'minutes';
+    return isSitting
+      ? `Sit down in: ${minutes} ${minuteText}`
+      : `Stand up in: ${minutes} ${minuteText}`;
+  };
+
+  function updateStatusBar(minutes: number, isSitting: boolean): void {
+    if (statusBarItem) {
+      statusBarItem.text = getStatusBarText(minutes, isSitting);
+      statusBarItem.show();
+    } else {
+      statusBarItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left,
+        100
+      );
+      statusBarItem.text = getStatusBarText(minutes, isSitting);
+      statusBarItem.show();
+    }
+  }
+
+  function clearStatusBar(): void {
+    if (statusBarItem) {
+      statusBarItem.hide();
+    }
+  }
+
+  function updateSitDownAlertTimer(): void {
+    if (standingTimer) {
+      clearTimeout(standingTimer);
+    }
+    standingTimer = setTimeout(() => {
+      vscode.window.showInformationMessage(
+        `It's time to sit down! - @${getCurrentDateTime()}`
+      );
+      isAlreadyStanding = false;
+      resetStandingUpTimer();
+    }, sittingInterval * 60 * 1000); // Convert sittingInterval to milliseconds
+  }
+
+  function startStandingTimer(): void {
+    if (!isAlreadyStanding) {
+      isAlreadyStanding = true;
+      updateSitDownAlertTimer();
+    }
+  }
+
+  function resetStandingUpTimer(): void {
+    if (standingUpInterval) {
+      clearInterval(standingUpInterval);
+    }
+
+    let remainingMinutes = isUserActive
+      ? standingInterval
+      : Math.max(
+          0,
+          standingInterval - Math.floor((Date.now() - lastActiveTime) / 60000)
+        );
+    updateStatusBar(remainingMinutes, isAlreadyStanding);
+
+    standingUpInterval = setInterval(() => {
+      if (isUserActive) {
+        remainingMinutes--;
+        updateStatusBar(remainingMinutes, isAlreadyStanding);
+        if (remainingMinutes === 0) {
+          //current time in readable format
+
+          vscode.window.showInformationMessage(
+            `It's time to stand up! - @${getCurrentDateTime()}`
+          );
+          clearStatusBar();
+          startStandingTimer(); // Start the standing timer when the user stands up
+          resetStandingUpTimer();
+        }
+      }
+      if (!isUserActive) {
+        if (Date.now() - lastActiveTime > INACTIVE_THRESHOLD * 60000) {
+          clearInterval(standingUpInterval);
+        }
+      }
+    }, 60 * 1000); // Update the status bar every 1 minute
+  }
+
+  vscode.workspace.onDidChangeTextDocument(() => {
+    isUserActive = true;
+    lastActiveTime = Date.now();
+    clearStatusBar();
+    resetStandingUpTimer();
+    if (isAlreadyStanding) {
+      updateSitDownAlertTimer();
+    }
+  });
+
+  vscode.window.onDidChangeTextEditorSelection(() => {
+    isUserActive = true;
+    lastActiveTime = Date.now();
+    clearStatusBar();
+    resetStandingUpTimer();
+    if (isAlreadyStanding) {
+      updateSitDownAlertTimer();
+    }
+  });
+
+  resetStandingUpTimer();
+
+  vscode.workspace.onDidChangeConfiguration((event) => {
+    if (event.affectsConfiguration('climbr.sittingInterval')) {
+      sittingInterval =
+        vscode.workspace.getConfiguration().get('climbr.sittingInterval') ??
+        DEFAULT_SITTING_INTERVAL;
+      clearStatusBar();
+    }
+    if (event.affectsConfiguration('climbr.standingInterval')) {
+      standingInterval =
+        vscode.workspace.getConfiguration().get('climbr.standingInterval') ??
+        DEFAULT_STANDING_INTERVAL;
+      resetStandingUpTimer();
+    }
+  });
+
+  context.subscriptions.push({
+    dispose: () => {
+      if (standingUpInterval) {
+        clearInterval(standingUpInterval);
+      }
+      clearStatusBar();
+    },
+  });
+
+  let disposable = vscode.commands.registerCommand(
+    'climbr.startReminder',
+    () => {
+      resetStandingUpTimer();
+      vscode.window.showInformationMessage(
+        `Stand-up reminder started! - @${getCurrentDateTime()}`
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
+
+  vscode.commands.executeCommand('climbr.startReminder');
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
